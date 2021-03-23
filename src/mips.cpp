510681072e4a311ff4c853b19005ac8866ed8d4f
u59633 <u59633@login-2.aidevcloud>
2021-03-22 17:47:56 -0700

    changed name method

--- src/mips.cpp
+++ src/mips.cpp
@@ -31,59 +31,36 @@ namespace priscas
 		REGISTERS reg_val
 			=
 			// Can optimize based off of 
-			fr_name[1] == 'a' ?
-				!strcmp("$a0", fr_name) ? $a0 :
-				!strcmp("$a1", fr_name) ? $a1 :
-				!strcmp("$a2", fr_name) ? $a2 :
-				!strcmp("$a3", fr_name) ? $a3 :
-				!strcmp("$at", fr_name) ? $at : INVALID
+			fr_name[1] == 'b' ?
+				!strcmp("$bp", fr_name) ? $bp : INVALID
 			:
 
 			fr_name[1] == 'f' ?
-				!strcmp("$fp", fr_name) ? $fp : INVALID
+				!strcmp("$flg", fr_name) ? $flg : INVALID
 			:
 
 			fr_name[1] == 'g' ?
-				!strcmp("$gp", fr_name) ? $gp : INVALID
-			:
+				!strcmp("$g1", fr_name) ? $g1 :
+				!strcmp("$g2", fr_name) ? $g2 :
+				!strcmp("$g3", fr_name) ? $g3 :
+				!strcmp("$g4", fr_name) ? $g4 :
+				!strcmp("$g5", fr_name) ? $g5 :
+				!strcmp("$g6", fr_name) ? $g6 :
+				!strcmp("$g7", fr_name) ? $g7 :
+				!strcmp("$g8", fr_name) ? $g8 :
+				!strcmp("$g9", fr_name) ? $g9 :
+				!strcmp("$g10", fr_name) ? $g10 :
+				!strcmp("$g11", fr_name) ? $g11: INVALID
 
 			fr_name[1] == 'k' ?
 				!strcmp("$k0", fr_name) ? $k0 :
 				!strcmp("$k1", fr_name) ? $k1 : INVALID
 			:
-
-			fr_name[1] == 'r' ?
-				!strcmp("$ra", fr_name) ? $ra : INVALID
+			fr_name[1] == 'p' ?
+				!strcmp("$pc", fr_name) ? $pc : INVALID
 			:
-
 			fr_name[1] == 's' ?
-				!strcmp("$s0", fr_name) ? $s0 :
-				!strcmp("$s1", fr_name) ? $s1 :
-				!strcmp("$s2", fr_name) ? $s2 :
-				!strcmp("$s3", fr_name) ? $s3 :
-				!strcmp("$s4", fr_name) ? $s4 :
-				!strcmp("$s5", fr_name) ? $s5 :
-				!strcmp("$s6", fr_name) ? $s6 :
-				!strcmp("$s7", fr_name) ? $s7 :
 				!strcmp("$sp", fr_name) ? $sp : INVALID
-			:
-
-			fr_name[1] == 't' ?
-				!strcmp("$t0", fr_name) ? $t0 :
-				!strcmp("$t1", fr_name) ? $t1 :
-				!strcmp("$t2", fr_name) ? $t2 :
-				!strcmp("$t3", fr_name) ? $t3 :
-				!strcmp("$t4", fr_name) ? $t4 :
-				!strcmp("$t5", fr_name) ? $t5 :
-				!strcmp("$t6", fr_name) ? $t6 :
-				!strcmp("$t7", fr_name) ? $t7 :
-				!strcmp("$t8", fr_name) ? $t8 :
-				!strcmp("$t9", fr_name) ? $t9 : INVALID
-			:
-
-			fr_name[1] == 'v' ?
-				!strcmp("$v0", fr_name) ? $v0 :
-				!strcmp("$v1", fr_name) ? $v1 : INVALID
 			:
 			fr_name[1] == 'z' ?
 				!strcmp("$zero", fr_name) ? $zero : INVALID
