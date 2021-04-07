46f48452b9d7ac247ae3093d947b8d5619d0b366
u59633 <u59633@login-2.aidevcloud>
2021-04-06 18:45:21 -0700

    started assembly code

---
+++ src/asmCode.asm
@@ -0,0 +1,42 @@
+//R0 zero register
+//R1-R15 general purpose registers g0-g14
+
+
+
+code_entry:
+	ADDI g0, R0, 1//Set g0 to one
+	ADDI g14, R0, 0x0 // Use g14 to be current value to hash
+	
+	STI g14, 1064 //load value to be hashed into 
+	ADDI g14, g14, 1 //Increament hash number
+	STI g14, 1164
+	ADDI g14, g14, 1
+	STI g14, 2064
+	ADDI g14, g14, 1
+	STI g14, 2164
+	ADDI g14, g14, 1
+	STI g14, 3064
+	ADDI g14, g14, 1
+	STI g14, 3164
+	ADDI g14, g14, 1
+	STI g14, 0x4064
+	ADDI g14, g14, 1
+	STI g14, 0x4164
+	
+
+	//Tell all accelerators to begin
+	STI g0, 0x1000
+	STI g0, 0x1100
+	STI g0, 0x2000
+	STI g0, 0x2100
+	STI g0, 0x3000
+	STI g0, 0x3100
+	STI g0, 0x4000
+	STI g0, 0x4100 
+
+
+
+loop_begin
+	LDI g0, 0x1001 //Status register for accelerator 1
+	SUBI g1, g0, 1//Check if accelerator done
+	BNEQ 
