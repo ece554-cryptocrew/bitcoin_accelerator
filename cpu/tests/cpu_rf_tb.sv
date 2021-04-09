2d0226850b4ed41ee3916106bd1b75202e9faf6d
logansisel <56094407+logansisel@users.noreply.github.com>
2021-04-08 20:13:36 -0500

    RF tb now passes, uses pos clk + 1, not sure if acceptable

--- cpu/tests/cpu_rf_tb.sv
+++ cpu/tests/cpu_rf_tb.sv
@@ -8,20 +8,11 @@ module cpu_rf_tb();
     logic        err;
 
     integer errs;
-    integer i;
+    integer i, j;
 
-    reg [31:0] mock_rf [0:15];
+    logic [31:0] mock_rf [0:15];
 
-    cpu_rf DUT(.clk(clk), 
-               .rst_n(rst_n),  
-               .sel1(sel1),  
-               .sel2(sel2),  
-               .wrt_sel(wrt_sel),  
-               .wrt_data(wrt_data),  
-               .wrt_en(wrt_en),  
-               .reg1(reg1),  
-               .reg2(reg2),  
-               .err(err));
+    cpu_rf rf(.clk(clk), .rst_n(rst_n), .sel1(sel1), .sel2(sel2), .wrt_sel(wrt_sel), .wrt_data(wrt_data), .wrt_en(wrt_en), .reg1(reg1), .reg2(reg2), .err(err));
 
     initial begin
         clk = 1'b0;
@@ -36,20 +27,37 @@ module cpu_rf_tb();
             mock_rf[i] = 32'h0;
         end
 
-        @(negedge clk);
+        @(posedge clk);
         rst_n = 1'b0;
-        @(negedge clk);
+        @(posedge clk);
         rst_n = 1'b1;
         
         for (i = 0; i < 100; i=i+1) begin
-            @(negedge clk);
+            $display("------------------------------------");
+            //Dump registers
+            for (j = 0; j < 16; j=j+1) begin
+                sel1 = j;
+                @(posedge clk); //TODO: Why do we need >1 clk cycle? Problem? Its a synchronous issue, idk
+                #1;
+                $display("Mock: Reg %0h: %0h", j, mock_rf[j]);
+                $display("Real: Reg %0h: %0h", j, reg1);
+            end
+            $display("------------------------------------");
+
+            @(posedge clk);
             sel1 = $random();
             sel2 = $random();
             wrt_sel = $random();
             wrt_data = $random();
             wrt_en = $random(); 
-            $display("%0d", i);
-
+            @(posedge clk); //TODO: Why do we need >1 clk cycle? Problem?
+            #1;
+            if (wrt_en) begin
+                mock_rf[wrt_sel] = wrt_data;
+                $display("Writing to reg %0h, data = %0h", wrt_sel, wrt_data);
+            end
+            @(posedge clk); //TODO: Why do we need >1 clk cycle? Problem?
+            #1;
             assert (reg1 == mock_rf[sel1]) $display("reg1 good: reg = %0h data = %0h", sel1, reg1);
                 else begin
                     $display("ERROR: REG 1 BAD DATA: reg = %0h data = %0h expecteddata = %0h", sel1, reg1, mock_rf[sel1]);
@@ -60,12 +68,7 @@ module cpu_rf_tb();
                     $display("ERROR: REG 2 BAD DATA: reg = %0h data = %0h expecteddata = %0h", sel2, reg2, mock_rf[sel2]);
                     errs++;
                 end
-            @(negedge clk);
-            if (wrt_en) begin
-                mock_rf[wrt_sel] = wrt_data;
-                $display("Writing to reg %0h, data = %0h", wrt_sel, wrt_data);
-            end
-            
+            @(posedge clk);
         end
         
         
