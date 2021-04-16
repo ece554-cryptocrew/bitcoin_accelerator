1936a4e6efba722f1f43df9371e2bfa6c8ebbbcc
logansisel <56094407+logansisel@users.noreply.github.com>
2021-04-15 19:27:46 -0500

    implemented a pipe reg module, parametrizable

--- cpu/rtl/cpu_pipereg.sv
+++ cpu/rtl/cpu_pipereg.sv
@@ -1,25 +1,26 @@
 /////////////////////////////////////////////////////////////////////////////////////
 //
-// Module: cpu_pc
+// Module: cpu_pipereg
 //
 // Author: Logan Sisel
 //
-// Detail: Contains the program counter.
-//         Initializes to 0x0000.
-//         pc_next is calculated at top level.        
+// Detail: Parametrizable width register for pipelines        
 //
 /////////////////////////////////////////////////////////////////////////////////////
-module cpu_pc(clk, rst_n, pc_next, pc_out);
+module cpu_pipereg(clk, rst_n, pipe_in, pipe_out, pipe_en);
+    
+    parameter PIPE_WIDTH = 32;
+
     input             clk, rst_n;
-    input      [15:0] pc_next;
-    output reg [15:0] pc_out;
+    input      [PIPE_WIDTH-1:0] pipe_in;
+    output reg [PIPE_WIDTH-1:0] pipe_out;
 
     always_ff @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
-            pc_out <= 16'h0;
+            pipe_out <= {PIPE_WIDTH{1'b0}};
         end
         else begin
-            pc_out <= pc_next;
+            if (pipe_en) pipe_out <= pipe_in;
         end
     end
 
